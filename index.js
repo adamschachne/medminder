959b3a4b8590eeb3a69bee24dc32c325952f9882
Gilberto Acevedo <giaceved@ucsd.edu>
2017-10-29 12:22:49 -0700

    Completed homescreen page and added links

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var medPage = require(__dirname + '/public/javascript/medication_page');
+var scheduleMedPage = require(__dirname + '/public/javascript/schedule_page');
+var settingsPage = require(__dirname + '/public/javascript/settings_page');
+var remindersPage = require(__dirname + '/public/javascript/reminders_page');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,8 +14,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/index', medPage);
+});
+app.get('/med/new', function(request, response) {
+  response.render('pages/new_med', scheduleMedPage);
+});
+app.get('/settings', function(request, response) {
+  response.render('pages/settings', settingsPage);
+});
+app.get('/reminders', function(request, response) {
+  response.render('pages/reminders', remindersPage);
 });
 
 app.listen(app.get('port'), function() {
