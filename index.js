5b0a99d9fe41e8584889d7a1575e1d047ae93345
Adam Schachne <adamschachne@live.com>
2017-11-06 22:13:59 -0800

    Database URL from process env

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var session = require('express-session')
 var hash = require('pbkdf2-password')()
 const KnexSessionStore = require('connect-session-knex')(session);
 const connection = new Client({
-  connectionString: 'postgres://byeqqktcpejzsj:9818338064bc71a71b172a94fd4bcf84cd307e6246a859c113b52e96e79628d1@ec2-50-19-105-113.compute-1.amazonaws.com:5432/d65ul1majensrg',
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 //db.connect();
