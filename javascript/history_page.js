66de5393d80203d7f70a9955999ec36f0f20c41e
Gilberto Acevedo <giaceved@ucsd.edu>
2017-11-10 22:01:37 -0800

    Implemented UI for history page

--- javascript/history_page.js
+++ javascript/history_page.js
@@ -1,3 +1,25 @@
 module.exports = {
-  page_title: 'History'
+  page_title: 'History',
+  data:
+    [{
+      med_name: "Synthroid",
+      days: ['M','W','F'],
+      repeat: 6
+    },
+    {
+      med_name: "Crestor",
+      days: ['T','Th'],
+      repeat: 2
+    },
+    {
+      med_name: "Ventolin HFA",
+      days: ['Su','M','T','W','Th','F','Sa'],
+      repeat: 3
+    },
+    {
+      med_name: "Nexium",
+      days: ['Su','M','T','W','Th','F','Sa'],
+      repeat: 4
+    }
+  ]
 }
